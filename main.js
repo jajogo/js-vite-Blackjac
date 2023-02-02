0ac40f89fdcc2079a98787b058719662da281334
James Gomez <jajogo502@gmail.com>
2023-02-02 15:04:29 -0500

    first Commit

---
+++ main.js
@@ -0,0 +1,3 @@
+import './src/blackjack';
+// si le pongo index.js no necesito buscarlo, porq lo buscara por defecto
+import './style.css';
---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,9 @@
+// import { crearDeck as crearNuevoDeck } from './usecases/crear-deck';
+// import {crearDeck,  tarea } from './usecases/crear-deck';
+// import crearDeck from './usecases/crear-deck';
+
+export { crearCartaHTML } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
